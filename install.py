85217f936dea7a01c4683447b8f7d1793b283fec
Jerome Lambourg <lambourg@adacore.com>
2017-04-21 16:21:37 +0200

    Improve install.py
    * Better output readability
    * Returns an error status in case a runtime has not been built
    * Cleanup the old runtime if it exists

--- install.py
+++ install.py
@@ -9,10 +9,13 @@ import getopt
 from glob import glob
 import os
 import re
+import shutil
+import stat
 import subprocess
 import sys
 
 def usage():
+    """Script usage"""
     print "usage: install.py [--arch=arm-eabi|aarch64-elf] [--prefix=<path>]"
     print "  --arch: only build for the specified architecture"
     print "  --prefix: installation prefix for the runtimes"
@@ -21,7 +24,12 @@ def usage():
     print "  Builds and installs all targets for which a compiler is available."
     print "  The runtimes are installed in the toolchain itself."
 
+
 def which(program):
+    """Finds 'program' on the path, and returns its absolute path.
+
+    Returns None if not found.
+    """
     paths = os.environ['PATH'].split(os.pathsep)
     if sys.platform == 'win32':
         (base, ext) = os.path.splitext(executable)
@@ -33,8 +41,55 @@ def which(program):
             return f
     return None
 
+
+def abspath(path):
+    """Returns the absolute path of 'path', relative to the repository"""
+    if os.path.isabs(path):
+        return path
+    else:
+        pwd = os.path.dirname(os.path.abspath(__file__))
+        return os.path.join(pwd, path)
+
+
+def rmtree(path):
+    def del_rw(action, name, exc):
+        os.chmod(name, stat.S_IWRITE)
+        os.remove(name)
+    shutil.rmtree(path, onerror=del_rw)
+
+
+def run_program(argv):
+    exe = os.path.basename(argv[0])
+    print "[%s] %s" % (exe, " ".join(argv[1:]))
+    p = subprocess.Popen(
+        argv,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+    stdout, stderr = p.communicate()
+
+    try:
+        stdout = stdout.decode('ascii')
+    except UnicodeError:
+        return 'stdout is not ASCII'
+
+    try:
+        stderr = stderr.decode('ascii')
+    except UnicodeError:
+        return 'stderr is not ASCII'
+
+    if len(stderr) > 0:
+        print stderr
+    if len(stdout) > 0:
+        print stdout
+
+    return p.returncode
+
+
 def build(archs, prefix):
-    projects = glob(os.path.join('BSPs', '*.gpr'))
+    projects = glob(abspath(os.path.join('BSPs', '*.gpr')))
+    overall_check = True
+
     for gpr in projects:
         # retrieve the rts target compiler
         with open(gpr, 'r') as fp:
@@ -58,22 +113,55 @@ def build(archs, prefix):
 
         if gcc_bin is None:
             print "skip %s: no compiler found for target %s" % (
-                gpr, target)
+                os.path.basename(gpr), target)
             continue
+
+        print '** %s **' % os.path.basename(gpr)
 
         gcc_dir = os.path.dirname(gcc_bin)
         gprbuild = os.path.join(gcc_dir, 'gprbuild')
         gprinstall = os.path.join(gcc_dir, 'gprinstall')
 
-        cmd = [gprbuild, '-P', gpr, '-j0', '-q']
-        print ' '.join(cmd)
-        subprocess.call(cmd, stdout=sys.stdout, stderr=sys.stderr, shell=False)
+        # clean-up the old rts if any
+        if prefix is not None:
+            rts_dir = prefix
+        else:
+            gcc_base = os.path.abspath(os.path.join(gcc_dir, os.pardir))
+            rts_dir = os.path.join(gcc_base, target, 'lib', 'gnat')
+        rts_name = os.path.basename(gpr).replace('_', '-').replace('.gpr', '')
+        rts_path = os.path.join(rts_dir, rts_name)
+        if os.path.isdir(rts_path):
+            print '[rmtree] %s' % rts_path
+            rmtree(rts_path)
+        else:
+            print "not found: %s" % rts_path
 
-        cmd = [gprinstall, '-P', gpr, '-p', '-f', '-q']
+        cmd = [gprbuild, '-P', gpr, '-p', '-q', '-j0']
+        error = False
+        returncode = run_program(cmd)
+        if returncode:
+            print 'Build error (gprbuild returned {}):\n{}'.format(
+                returncode, stderr)
+            overall_check = False
+            error = True
+            continue
+
+        cmd = [gprinstall, '-P', gpr, '-p', '-q', '-f']
         if prefix is not None:
             cmd += ['-XPREFIX=%s' % prefix]
-        print ' '.join(cmd)
-        subprocess.call(cmd, stdout=sys.stdout, stderr=sys.stderr, shell=False)
+        returncode = run_program(cmd)
+        if returncode:
+            print 'Build error (gprinstall returned {}):\n{}'.format(
+                returncode, stderr)
+            overall_check = False
+            error = True
+            continue
+
+        if error:
+            print "Failed"
+        else:
+            print "OK"
+        print ""
 
 
 def main():
